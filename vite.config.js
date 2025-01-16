568eac9e2e6a005db78a790337697bfbfa7375aa
Vinay Panwar <vinaypanwar280@gmail.com>
2025-01-16 19:10:22 +0000

    FEAT: Used the base path for app.

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], 
-  build: {
-    assetsDir: 'Turf', // Set the assets directory to include your prefix
-  },
+  plugins: [react()],
+  base: "/Turf/"
 })
